98b1d9cccec70c0917357454d61268b43a9608ae
Zimeng Xiong (asahi-air) <zxzimeng@gmail.com>
2025-03-04 22:48:34 -0800

    update shasum

--- mm.rb
+++ mm.rb
@@ -1,8 +1,8 @@
 class Mm < Formula
   desc "A simple molecular weight calculator implemented in Rust"
   homepage "https://github.com/zxzimeng/cli-molecular-weight-calculator"
-  url "https://github.com/zxzimeng/cli-molecular-weight-calculator/archive/v0.1.1.tar.gz"
-  sha256 "60a2fa77e96f4f8b28cd83f77caa32eb95f1d3dfec2c88888d77fed336ec1d5b"
+  url "https://github.com/zxzimeng/cli-molecular-weight-calculator/releases/download/v0.1.1/v0.1.1.tar.gz"
+  sha256 "fe57541c6a263e4518e999024cd98cf016a9eb2b4dd559eea491c96868ea0dd3"
   license "MIT"
 
   depends_on "rust" => :build
